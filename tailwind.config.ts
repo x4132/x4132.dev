e29113791a72324dcc96a1a50d5f1b465e8712ba
x4132 <31595285+x4132@users.noreply.github.com>
2025-02-28 21:58:24 -0800

    initial t3/next commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,70 +1,14 @@
-import type { Config } from "tailwindcss";
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
-const config: Config = {
-  darkMode: "selector",
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+export default {
+  content: ["./src/**/*.tsx"],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       fontFamily: {
-        body: [
-          "Gilroy",
-          "ui-sans-serif",
-          "system-ui",
-          "-apple-system",
-          "Segoe UI",
-          "Roboto",
-          "Helvetica Neue",
-          "Arial",
-          "Noto Sans",
-          "Liberation Sans",
-          "sans-serif",
-          "Apple Color Emoji",
-          "Segoe UI Emoji",
-          "Segoe UI Symbol",
-          "Noto Color Emoji",
-        ],
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
-      colors: {
-        primary: "#42A5F5",
-        secondary: "#4FC3F7",
-        tertiary: "#2196F3",
-        dark: "#000000",
-        light: "#FFFFFF",
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "spin-slow": "spin 3s linear infinite",
-        "spin-slower": "spin 6s linear infinite",
-      },
-      rotate: {
-        "330": "330deg",
-        "30": "30deg"
-      }
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
-};
-export default config;
+  plugins: [],
+} satisfies Config;
